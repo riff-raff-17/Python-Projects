ae80026af92a60bc387376646b001c210a4ae373
riff-raff-17 <65214797+riff-raff-17@users.noreply.github.com>
2025-03-21 13:40:25 +0800

    Added NEAT dinosaur game project

---
+++ Finished Projects/NEAT dino/part1.py
@@ -0,0 +1,97 @@
+# Create the dinosaur class and make the dinosaur jump
+# The dinosaur can jump but there is no background or score
+
+#1_______________________________________________________________________________________________________________________
+
+import pygame
+import os
+import random
+import sys
+
+pygame.init()
+
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1100
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+RUNNING = [pygame.image.load(os.path.join("Assets/Dino", "DinoRun1.png")),
+           pygame.image.load(os.path.join("Assets/Dino", "DinoRun2.png"))]
+
+JUMPING = pygame.image.load(os.path.join("Assets/Dino", "DinoJump.png"))
+
+FONT = pygame.font.Font('freesansbold.ttf', 20)
+
+BG = pygame.image.load(os.path.join("Assets/Other", "Track.png"))
+
+#2_______________________________________________________________________________________________________________________
+class Dinosaur:
+    X_POS = 80
+    Y_POS = 310
+    JUMP_VEL = 8
+
+    def __init__(self, img=RUNNING[0]):
+        self.image = img
+        self.dino_run = True
+        self.dino_jump = False
+        self.jump_vel = self.JUMP_VEL
+        self.rect = pygame.Rect(self.X_POS, self.Y_POS, img.get_width(), img.get_height())
+        self.step_index = 0
+
+    def update(self):
+        if self.dino_run:
+            self.run()
+        if self.dino_jump:
+            self.jump()
+        if self.step_index >= 10:
+            self.step_index = 0
+
+    def jump(self):
+        self.image = JUMPING
+        if self.dino_jump:
+            self.rect.y -= self.jump_vel * 4
+            self.jump_vel -= 0.8
+        if self.jump_vel <= -self.JUMP_VEL:
+            self.dino_jump = False
+            self.dino_run = True
+            self.jump_vel = self.JUMP_VEL
+
+    def run(self):
+        self.image = RUNNING[self.step_index // 5]
+        self.rect.x = self.X_POS
+        self.rect.y = self.Y_POS
+        self.step_index += 1
+
+    def draw(self, SCREEN):
+        SCREEN.blit(self.image, (self.rect.x, self.rect.y))
+        
+#1_______________________________________________________________________________________________________________________
+
+def main():
+    clock = pygame.time.Clock() 
+
+    dinosaurs = [Dinosaur()]
+    
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        SCREEN.fill((255, 255, 255))
+
+        for dinosaur in dinosaurs:
+            dinosaur.update()
+            dinosaur.draw(SCREEN)
+
+        user_input = pygame.key.get_pressed()
+
+        for i, dinosaur in enumerate(dinosaurs):
+            if user_input[pygame.K_SPACE] and not dinosaur.dino_jump:
+                dinosaur.dino_jump = True
+                dinosaur.dino_run = False
+
+        clock.tick(60)
+        pygame.display.update()
+
+main()
